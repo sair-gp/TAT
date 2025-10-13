caecda8198006e063d652752e8ebb2f32c6a1f7c
sair-gp <sair.bermudez@protonmail.com>
2025-10-12 21:27:28 -0400

    Finishing the useCountdown hook (I had no experience making custom hooks nor working with typescript lol)

---
+++ src/hooks/useCountdown.tsx
@@ -0,0 +1,51 @@
+import { useEffect, useState, useRef } from "react";
+
+interface useCountdownProps {
+  duration: number;
+  autoStart?: boolean;
+}
+
+export const useCountdown = ({ duration, autoStart }: useCountdownProps) => {
+  const [timeLeft, setTimeLeft] = useState(duration);
+  const [isRunning, setIsRunning] = useState(autoStart ?? false);
+  // using a ref to ensure access to the interval id
+  const intervalRef = useRef<number | undefined>(undefined);
+  //const timeRef = useRef<ReturnType<typeof setInterval> | null>(null);
+
+  //Changes the state to start or stop the timer by using the updater function (prev)
+  const handleStartStop = () => {
+    //interval cleanup (just in case)
+    if (intervalRef.current) clearInterval(intervalRef.current);
+    setIsRunning((prev) => !prev);
+  };
+
+  const handleReset = () => {
+    // Interval cleanup. Necessary for reseting the timer
+    if (intervalRef.current) clearInterval(intervalRef.current);
+    setTimeLeft(duration);
+    setIsRunning(true);
+  };
+
+  useEffect(() => {
+    if (isRunning === false) {
+      return;
+    }
+    intervalRef.current = setInterval(() => {
+      setTimeLeft((prevTime) => {
+        if (prevTime === 0) {
+          // Cleaning the interval and returning to ensure the timer stops at 0 and doesn't go down to -1
+          clearInterval(intervalRef.current);
+          return 0;
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
+
+    return () => {
+      // Cleanup
+      if (intervalRef.current) clearInterval(intervalRef.current);
+    };
+  }, [isRunning]); // isRunning is set as a dependency so useEffect is executed everytime isRunning's value is updated
+
+  return [timeLeft, handleStartStop, handleReset, isRunning];
+};
